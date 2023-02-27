97ae403ab58dc0efcd7e39ca01000e215f722788
ajmal <ahoyaju@gmail.com>
2023-02-27 20:42:13 +0530

    initial commit

---
+++ cover/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class HomeView(TemplateView):
+    template_name="home.html"
+
+class servicesView(TemplateView):
+    template_name="ourservices.html"
+
+class AboutView(TemplateView):
+    template_name="aboutme.html"
